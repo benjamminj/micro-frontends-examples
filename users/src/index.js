5071efe4df151a0d9fc00e5a2c49d9021d10de11
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-07-28 16:54:33 -0700

    Add users micro-frontend

--- users/src/index.js
+++ users/src/index.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
 const path = require('path');
-const proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 
-app.use('/a', proxy(process.env.POSTS_URL))
-app.use('/b', proxy(process.env.B_URL))
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, './index.html')))
+app.use(express.static(path.join(__dirname, '/imports')))
+
+// Send down a basic html for the /posts.html for simpler testing
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
