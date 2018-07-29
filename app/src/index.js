cf99a07fe16c247babe5efa1085f3c7e976a3fac
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-07-28 18:19:28 -0700

    Update folder structure & add basic orchestration commands

--- app/src/index.js
+++ app/src/index.js
@@ -5,10 +5,11 @@ const proxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 
-app.use('/a', proxy(process.env.POSTS_URL))
+app.use('/a', proxy(process.env.A_URL))
 app.use('/b', proxy(process.env.B_URL))
 app.use('/c', proxy(process.env.C_URL))
+
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, './index.html')))
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT_APP || 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- micro-frontends/b/src/index.js
+++ micro-frontends/b/src/index.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, '/imports')))
 // Send down a basic html for the /posts.html for simpler testing
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT_B || 5000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
