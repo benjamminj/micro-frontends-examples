806b710bd58ec26344119063e7ddfff9f2395eda
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-07-28 19:21:11 -0700

    Fix dev script & port numbers

--- app/src/index.js
+++ app/src/index.js
@@ -11,5 +11,9 @@ app.use('/c', proxy(process.env.C_URL))
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, './index.html')))
 
-const port = process.env.PORT_APP || 5000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+  // TODO -- better way to say the message
+  console.log(`You can view the sample orchestration app at http://localhost:${port}`)
+})
