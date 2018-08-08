174c1b8ecfeb0e6041362674e6c957cc7d0c8bbf
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-08-07 22:12:00 -0700

    Prettify code

--- app/index.js
+++ app/index.js
@@ -1,20 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const path = require('path');
-const proxy = require('express-http-proxy');
-const express = require('express');
+const path = require("path");
+const proxy = require("express-http-proxy");
+const express = require("express");
 const app = express();
 
-app.use('/a', proxy(process.env.A_URL))
-app.use('/b', proxy(process.env.B_URL))
-app.use('/c', proxy(process.env.C_URL))
+app.use("/a", proxy(process.env.A_URL));
+app.use("/b", proxy(process.env.B_URL));
+app.use("/c", proxy(process.env.C_URL));
 
-app.get('/', (req,res) => res.sendFile(path.join(__dirname, './index.html')))
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-  // TODO -- better way to say the message
-  console.log(`You can view the sample orchestration app at http://localhost:${port}`)
-})
+app.listen(port, () => `Listening for connections on port ${port}`);
